1a4871db2df9c81501a9b47a615aa88ecba6625c
mg-skc <71731493+mg-skc@users.noreply.github.com>
2021-02-04 17:16:45 -0600

    major changes: jwt, register, login, change password, show password

--- login-server-v2.js
+++ login-server-v2.js
@@ -1,22 +1,16 @@
-// if (process.env.NODE_ENV !== 'production') {
-//     require('dotenv').config()
-// }
-
-
-
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
-const passport = require('passport')
+// const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
 const jwt = require ('jsonwebtoken')
 const jwtSimple = require('jwt-simple')
 const bodyParser = require("body-parser");
-//const initializePassport = require('./passport-config')
 const cookieParser = require('cookie-parser')
-//const { logIn, welcome, refresh } = require("./handlers")
+const JWT_Secret = process.env.JWT_SECRET;
 
 //require mongoose-translates Node.js
 const mongoose = require ("mongoose");
@@ -34,8 +28,8 @@ const path = require("path");
 const port = 3000;
 
 //connect to Atlas cluster
-const mongoDB = "mongodb+srv://wastenotskilledkc:madANDal4life2021@wastenot1.sj0ff.mongodb.net/WasteNot1?retryWrites=true&w=majority";
-
+const mongoLogIn = process.env.mongoInfo
+const mongoDB = 'mongodb+srv://'+ mongoLogIn;
 
 //accessing the connect method of mongoose
 //pass it the name of the DB cluster we have created
@@ -71,23 +65,115 @@ app.use(
 app.use(express.urlencoded({ extended: false}))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(flash())
+// app.use(flash())
 // app.use(session({
 //     secret: process.env.SESSION_SECRET,
 //     resave: false,
 //     saveUninitialized: false
 // }))
 //app.use(passport.initialize())
-app.use(passport.session())
+// app.use(passport.session())
 app.use(methodOverride('_method'))
 app.use(express.json())
 app.use (cookieParser())
 
+app.post('/register', async (req, res) =>{
+    console.log(req.body)
+
+    if((!req.body.name || typeof req.body.name != 'string')){
+        return res.json({status: 'error', error: 'Invalid name - name is a required field'})
+    }
+
+    if((!req.body.email || typeof req.body.email != 'string')){
+        return res.json({status: 'error', error: 'Invalid email - email is a required field.'})
+    }
+
+    if((!req.body.phone || typeof req.body.phone != 'string')){
+        return res.json({status: 'error', error: 'Invalid phone - phone is a required field.'})
+    }
+
+    if((!req.body.password || typeof req.body.password != 'string')){
+        return res.json({status: 'error', error: 'Invalid password - password is a required field.'})
+    }
+
+    if((req.body.password.length < 6)){
+        return res.json({status: 'error', error: 'Please use a password of at least 6 characters.'})
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password,10)
+    var newUser = {
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+        phone: req.body.phone,
+        user_role: "None"
+         }
+    try {
+        let user = new User(newUser)
+       const response = await user.save(newUser)
+       console.log('New User saved successfully:',response)
+       return res.json({status: 'ok'})
+
+    } catch(error){
+        console.log(JSON.stringify(error))
+        if(error.code === 11000) {
+            return res.json({status: 'error', error: "Email already in use - please use a different email address."})
+        }
+        throw(error) 
+    }
+})
+
+app.post('/login', async (req, res) =>{
+    console.log(req.body)
+    const {email, password} = req.body
+
+    const user = await User.findOne({email}).lean()
+
+    if(!user){
+        return res.json({status: 'error', error: 'Invalid username/password'})
+    }
+
+    if(await bcrypt.compare(password, user.password)){
+        const token = jwt.sign({
+            id: user._id, 
+            name:user.name
+        }, JWT_Secret)
 
 
+        return res.json({status: 'ok', data: token})
+    }
+     res.json({status: 'error', error: 'Invalid username/password'})
+
+})
+
+app.post('/change-password', async (req, res) =>{
+    console.log(req.body)
+    const {token, newpassword} = req.body
+
+    if((!req.body.newpassword || typeof req.body.password != 'string')){
+        return res.json({status: 'error', error: 'Invalid password - password is a required field.'})
+    }
+
+    if((req.body.newpassword.length < 6)){
+        return res.json({status: 'error', error: 'Please use a password of at least 6 characters.'})
+    }
 
 
+    try{
+    const user = jwt.verify(token, JWT_Secret)
+    console.log(user)
+    const _id = user.id
+    const hashedPassword = await bcrypt.hash(newpassword,10)
 
+    await User.updateOne({_id}, {
+        $set: {password: hashedPassword}
+    }
+        )
+    return res.json({status: 'ok'} )
+     
+    } catch (error){
+    res.json({status: 'error', error: 'Invalid token'})
+    }
 
 app.listen(3000,()=>{
     console.log('Server up at 3000')
